c89ad8699f7048ae9b4adba939538ec68758e8c7
UnrealTemplier <jaimore.evseev@gmail.com>
2025-05-22 20:21:56 +0400

    037 - Constructor property promotion.

--- src/Transaction.php
+++ src/Transaction.php
@@ -4,38 +4,14 @@ declare(strict_types=1);
 
 class Transaction
 {
-    private float $amount;
-    private string $description;
-
-    public function __construct(float $amount, string $description)
-    {
-        $this->amount = $amount;
-        $this->description = $description;
-    }
-
-    public function __destruct()
-    {
-        echo 'Destruct ' . $this->description . '<br/>';
-
-        // if exit called in a destructor
-        // then no other destructor will be called ever
-        //exit;
-    }
-
-    public function addTax(float $rate): Transaction
-    {
-        $this->amount += $this->amount * $rate / 100;
-        return $this;
-    }
-
-    public function addDiscount(float $rate): Transaction
-    {
-        $this->amount -= $this->amount * $rate / 100;
-        return $this;
-    }
-
-    public function getAmount(): float
-    {
-        return $this->amount;
+    public function __construct(
+        public float $amount,
+        public string $description = 'Default',
+        private ?string $name = null,
+    ) {
+        printLn($amount);
+        printLn($this->amount);
+        printLn($this->description);
+        var_dump($this->name);
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -5,68 +5,9 @@ declare(strict_types=1);
 require_once './../helpers.php';
 require_once '../Transaction.php';
 
-$json = '{"a": 1, "b": 2, "c": 3}';
-
-// decode JSON to associative array
-//$decode = json_decode($json, true);
-
-// decode JSON to stdClass object
-$decode = json_decode($json);
-
-var_dump($decode);
+new Transaction(15);
 
 printLn();
 printLn();
 
-// JSON keys can be used as object's properties
-// if we decoded JSON to stdClass object
-printLn($decode->a);
-printLn($decode->b);
-printLn($decode->c);
-
-printLn();
-printLn();
-
-// we can create empty stdClass object
-// and assign arbitrary properties
-$obj = new \stdClass();
-$obj->a = 10;
-$obj->b = 20;
-
-var_dump($obj);
-
-printLn();
-printLn();
-
-// we can convert array to stdClass object
-// by casting to object
-$arr = ['A', 'B', 'C'];
-$obj = (object)$arr;
-
-// cast object will receive array indices as keys
-var_dump($obj);
-
-printLn();
-
-// to use numeric keys on objects
-// we must surround them with curly braces
-printLn($obj->{1});
-
-
-printLn();
-printLn();
-
-// when we convert scalar value to stdClass object
-// then the key will be 'scalar'
-$obj1 = (object)1;
-$obj2 = (object)true;
-
-var_dump($obj1);
-printLn();
-var_dump($obj2);
-
-printLn();
-var_dump($obj1->scalar);
-
-printLn();
-var_dump($obj2->scalar);
+new Transaction(20, 'Custom description', 'Migele');
