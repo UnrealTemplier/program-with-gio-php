7454092b88f46d2fe076b08601b27b84e3471214
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-02 11:21:32 +0400

    041 - OOP: Statics tips and tricks.

--- src/app/PaymentGateway/Paddle/Transaction.php
+++ src/app/PaymentGateway/Paddle/Transaction.php
@@ -4,19 +4,37 @@ declare(strict_types=1);
 
 namespace App\PaymentGateway\Paddle;
 
-use App\Enums\Status;
-
 class Transaction
 {
     private static int $count = 0;
+
+    public int $amount = 25;
 
     public function __construct()
     {
         self::$count++;
     }
 
-    public static function getCount()
+    public static function getCount(): int
     {
         return self::$count;
+    }
+
+    public function process(): void
+    {
+        array_map(function () {
+            $this->amount = 35;
+        }, [1]);
+
+        echo 'Processing transaction non-statically<br/>';
+    }
+
+    public function processStatic(): void
+    {
+        array_map(static function () {
+            $this->amount = 45;
+        }, [1]);
+
+        echo 'Processing transaction statically<br/>';
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -5,20 +5,43 @@ declare(strict_types=1);
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-use App\DB;
+use App\PaymentGateway\Paddle\Transaction;
 
-// If we don't use singleton pattern
-// then every instantiation will call the constructor
-//$db = new DB([]);
-//$db = new DB([]);
-//$db = new DB([]);
-//$db = new DB([]);
-//$db = new DB([]);
+// We can implement Factory pattern using statics
+// to construct and return requested objects
+// Is's not working code, just for example.
 
-// If we use singleton pattern
-// then constructor will be called only once
-$db = DB::getInstance([]);
-$db = DB::getInstance([]);
-$db = DB::getInstance([]);
-$db = DB::getInstance([]);
-$db = DB::getInstance([]);
+// $transaction = TransactionFactory::make(1, 'Name');
+
+$transaction = new Transaction();
+// For static properties retrieval
+// we can use either :: or ->
+// but -> is not good 'cause it hides property's static nature
+var_dump($transaction::getCount());
+var_dump($transaction->getCount());
+
+print_line();
+print_line();
+
+print_line('Transaction amount:');
+var_dump($transaction->amount);
+
+print_line();
+print_line();
+
+
+// Using non-static closure we can modify
+// object's properties => not good...
+$transaction->process();
+print_line('Transaction amount:');
+var_dump($transaction->amount);
+
+print_line();
+print_line();
+
+// Using static closure we can't modify
+// object's properties => GOOD!
+// PHP will throw a fatal error
+$transaction->processStatic();
+print_line('Transaction amount:');
+var_dump($transaction->amount);
