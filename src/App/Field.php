dbeb81c49b45169191da10e85e6c496389488ed5
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-08 13:40:43 +0400

    044 - OOP: Abstract classes and methods.

---
+++ src/App/Field.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+abstract class Field
+{
+    public function __construct(public string $name) {}
+
+    abstract public function render(): string;
+}
--- src/public/index.php
+++ src/public/index.php
@@ -2,28 +2,17 @@
 
 declare(strict_types=1);
 
-use App\Toaster;
-use App\ToasterPro;
+use App\{Boolean, Checkbox, Field, Radio, Text};
 
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-// Choose base class of derived class
-// to test toast() function
+$fields = [
+    new Text('Text'),
+    new Checkbox('Checkbox'),
+    new Radio('Radio'),
+];
 
-//$toaster = new Toaster();
-$toaster = new ToasterPro('additional parameter');
-
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-
-toast($toaster);
-
-// This function receives all Toaster objects and Toaster descendants objects
-function toast(Toaster $toaster): void
-{
-    $toaster->toast();
+foreach ($fields as $field) {
+    echo $field->render() . '<br/>';
 }
