b79451402f7c3cf1a2cf507dae8543ea1ef03366
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-10 17:55:53 +0400

    050 - PHP variable storage, object comparison and zval.

--- src/public/index.php
+++ src/public/index.php
@@ -1,21 +1,76 @@
 <?php
 
-declare(strict_types=1);
-
-use App\ClassA;
+use App\Invoice;
 
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-$a = new ClassA(2, 5);
-var_dump($a);
+print_line('different objects, different values');
+$invoice1 = new Invoice(25, 'Invoice 1');
+$invoice2 = new Invoice(50, 'Invoice 2');
 
+var_dump($invoice1);
+print_line();
+var_dump($invoice2);
 print_line();
 
-$a->foo();
+var_dump($invoice1 == $invoice2);
+print_line();
+var_dump($invoice1 === $invoice2);
+
+print_line();
 print_line();
 
-$obj = $a->bar();
-var_dump($obj);
+// -------------------------------------
+
+print_line('different objects, same values');
+$invoice1 = new Invoice(100, 'Invoice');
+$invoice2 = new Invoice(100, 'Invoice');
+
+var_dump($invoice1);
 print_line();
-$obj->baz();
+var_dump($invoice2);
+print_line();
+
+var_dump($invoice1 == $invoice2);
+print_line();
+var_dump($invoice1 === $invoice2);
+
+print_line();
+print_line();
+
+// -------------------------------------
+
+print_line('different objects, different values, but (1 == true) if compared loose');
+$invoice1 = new Invoice(1, 'Invoice');
+$invoice2 = new Invoice(true, 'Invoice');
+
+var_dump($invoice1);
+print_line();
+var_dump($invoice2);
+print_line();
+
+var_dump($invoice1 == $invoice2);
+print_line();
+var_dump($invoice1 === $invoice2);
+
+print_line();
+print_line();
+
+// -------------------------------------
+
+print_line('same objects, different references');
+$invoice1 = new Invoice(100, 'Invoice');
+$invoice2 = $invoice1;
+
+var_dump($invoice1);
+print_line();
+var_dump($invoice2);
+print_line();
+
+var_dump($invoice1 == $invoice2);
+print_line();
+var_dump($invoice1 === $invoice2);
+
+print_line();
+print_line();
