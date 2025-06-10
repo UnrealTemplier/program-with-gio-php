0325f229f8bb13a3dfda4c7b33910093ecf9d989
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-10 18:06:35 +0400

    050 - Recursion and circular dependency in comparison - Fatal Error!

--- src/App/Invoice.php
+++ src/App/Invoice.php
@@ -4,8 +4,9 @@ namespace App;
 
 class Invoice
 {
+    public ?Invoice $linkedInvoice = null;
+
     public function __construct(
-        public Customer $customer,
         public float $amount,
         public string $description,
     ) {}
--- src/public/index.php
+++ src/public/index.php
@@ -1,32 +1,16 @@
 <?php
 
-use App\Customer;
 use App\Invoice;
 
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-print_line('different objects, same values, nested objects also have the same values');
-$invoice1 = new Invoice(new Customer('1'), 100, 'Invoice');
-$invoice2 = new Invoice(new Customer('1'), 100, 'Invoice');
+print_line('circular dependency!');
+$invoice1 = new Invoice(100, 'Invoice');
+$invoice2 = new Invoice(100, 'Invoice');
 
-var_dump($invoice1);
-print_line();
-var_dump($invoice2);
-print_line();
-
-var_dump($invoice1 == $invoice2);
-print_line();
-var_dump($invoice1 === $invoice2);
-
-print_line();
-print_line();
-
-// ----------------------------------
-
-print_line('different objects, same values, nested objects have different values');
-$invoice1 = new Invoice(new Customer('1'), 100, 'Invoice');
-$invoice2 = new Invoice(new Customer('2'), 100, 'Invoice');
+$invoice1->linkedInvoice = $invoice2;
+$invoice2->linkedInvoice = $invoice1;
 
 var_dump($invoice1);
 print_line();
