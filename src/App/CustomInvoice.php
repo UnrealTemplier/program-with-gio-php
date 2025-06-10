62c79fc41ee7cc60d7dacb73c4368edb7ea7ab7d
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-10 17:59:22 +0400

    050 - Different classes (though inherited) comparison.

---
+++ src/App/CustomInvoice.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace App;
+
+class CustomInvoice extends Invoice {}
--- src/public/index.php
+++ src/public/index.php
@@ -1,31 +1,14 @@
 <?php
 
+use App\CustomInvoice;
 use App\Invoice;
 
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-print_line('different objects, different values');
-$invoice1 = new Invoice(25, 'Invoice 1');
-$invoice2 = new Invoice(50, 'Invoice 2');
-
-var_dump($invoice1);
-print_line();
-var_dump($invoice2);
-print_line();
-
-var_dump($invoice1 == $invoice2);
-print_line();
-var_dump($invoice1 === $invoice2);
-
-print_line();
-print_line();
-
-// -------------------------------------
-
-print_line('different objects, same values');
+print_line('different classes, same values');
 $invoice1 = new Invoice(100, 'Invoice');
-$invoice2 = new Invoice(100, 'Invoice');
+$invoice2 = new CustomInvoice(100, 'Invoice');
 
 var_dump($invoice1);
 print_line();
@@ -35,42 +18,3 @@ print_line();
 var_dump($invoice1 == $invoice2);
 print_line();
 var_dump($invoice1 === $invoice2);
-
-print_line();
-print_line();
-
-// -------------------------------------
-
-print_line('different objects, different values, but (1 == true) if compared loose');
-$invoice1 = new Invoice(1, 'Invoice');
-$invoice2 = new Invoice(true, 'Invoice');
-
-var_dump($invoice1);
-print_line();
-var_dump($invoice2);
-print_line();
-
-var_dump($invoice1 == $invoice2);
-print_line();
-var_dump($invoice1 === $invoice2);
-
-print_line();
-print_line();
-
-// -------------------------------------
-
-print_line('same objects, different references');
-$invoice1 = new Invoice(100, 'Invoice');
-$invoice2 = $invoice1;
-
-var_dump($invoice1);
-print_line();
-var_dump($invoice2);
-print_line();
-
-var_dump($invoice1 == $invoice2);
-print_line();
-var_dump($invoice1 === $invoice2);
-
-print_line();
-print_line();
