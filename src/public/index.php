2c07e8fec5813e0be57d34f04914da0a6b4fc41f
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-11 20:14:59 +0400

    055 - DateTime: dates comparison.

--- src/public/index.php
+++ src/public/index.php
@@ -5,7 +5,20 @@ declare(strict_types=1);
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-$date = '15/03/2033';
-$dateTime = DateTime::createFromFormat('d/m/Y', $date)->setTime(0, 0);
+$date1 = new DateTime('05/25/2001 9:14AM');
+$date2 = new DateTime('05/25/2001 9:15AM');
 
-d($dateTime);
+d($date1 < $date2);
+d($date1 > $date2);
+d($date1 == $date2);
+d($date1 <=> $date2);
+
+print_line('------------------------');
+
+$date1 = new DateTime('05/25/2001 9:15AM');
+$date2 = new DateTime('05/25/2001 9:15AM');
+
+d($date1 < $date2);
+d($date1 > $date2);
+d($date1 == $date2);
+d($date1 <=> $date2);
