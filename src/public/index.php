4e46b51a3fe746de780a5753a6ee77905488e9b7
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-11 20:23:11 +0400

    055 - DateTime: diff.

--- src/public/index.php
+++ src/public/index.php
@@ -6,19 +6,9 @@ require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
 $date1 = new DateTime('05/25/2001 9:14AM');
-$date2 = new DateTime('05/25/2001 9:15AM');
+$date2 = new DateTime('03/15/2001 3:53AM');
 
-d($date1 < $date2);
-d($date1 > $date2);
-d($date1 == $date2);
-d($date1 <=> $date2);
-
-print_line('------------------------');
-
-$date1 = new DateTime('05/25/2001 9:15AM');
-$date2 = new DateTime('05/25/2001 9:15AM');
-
-d($date1 < $date2);
-d($date1 > $date2);
-d($date1 == $date2);
-d($date1 <=> $date2);
+print_line('Total days diff: ' . $date1->diff($date2)->days);
+print_line('Full diff: ' . $date1->diff($date2)->format('%Y years, %m months, %d days, %H hours, %i minutes, %s seconds'));
+print_line('Formatted total days diff: ' . $date1->diff($date2)->format('%a total days'));
+print_line('Formatted SIGNED total days diff: ' . $date1->diff($date2)->format('%R%a total days'));
