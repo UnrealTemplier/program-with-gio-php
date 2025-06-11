d67893959e2c82dd79966101f8f8b94a83c3bd68
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-11 20:35:32 +0400

    055 - DateTime: DateInterval and DateTimeImmutable.

--- src/public/index.php
+++ src/public/index.php
@@ -5,10 +5,36 @@ declare(strict_types=1);
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-$date1 = new DateTime('05/25/2001 9:14AM');
-$date2 = new DateTime('03/15/2001 3:53AM');
+$date = new DateTime('05/25/2001 9:14AM');
+$interval = new DateInterval('P3M');
 
-print_line('Total days diff: ' . $date1->diff($date2)->days);
-print_line('Full diff: ' . $date1->diff($date2)->format('%Y years, %m months, %d days, %H hours, %i minutes, %s seconds'));
-print_line('Formatted total days diff: ' . $date1->diff($date2)->format('%a total days'));
-print_line('Formatted SIGNED total days diff: ' . $date1->diff($date2)->format('%R%a total days'));
+print_line('Original: ' . $date->format('d.m.Y g:i'));
+
+$date->add($interval);
+print_line('Added 3 months: ' . $date->format('d.m.Y g:i'));
+
+$date->sub($interval);
+print_line('Subtracted 3 months: ' . $date->format('d.m.Y g:i'));
+
+$interval->invert = 1;
+$date->add($interval);
+print_line('Added 3 months INVERTED: ' . $date->format('d.m.Y g:i'));
+
+print_line('--------------------------');
+
+$interval->invert = 0;
+
+$from = new DateTimeImmutable();
+$to = (clone $from)->add($interval);
+
+print_line('from:');
+d($from);
+print_line('to:');
+d($to);
+
+print_line('from equal to to:');
+d($from === $to);
+
+print_line('to subbed:');
+$to = $to->sub($interval);
+d($to);
