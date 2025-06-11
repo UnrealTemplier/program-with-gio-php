520039b39dd62fd167eb21b1f019d1c9467c1b57
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-11 18:05:54 +0400

    054 - OOP error handling: return statements in try catch finally block.

--- src/public/index.php
+++ src/public/index.php
@@ -7,12 +7,20 @@ use App\Order;
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-$order1 = new Order(new Customer(), 25);
+function process(): int|bool
+{
+    $order1 = new Order(new Customer(), 25);
 
-try {
-    $order1->process();
-} catch (InvalidArgumentException|MissingBillingInfoException $e) {
-    print_line($e->getMessage() . ' ' . $e->getFile() . ':' . $e->getLine());
-} finally {
-    print_line('Finally!');
+    try {
+        $order1->process();
+        return true;
+    } catch (InvalidArgumentException|MissingBillingInfoException $e) {
+        print_line($e->getMessage() . ' ' . $e->getFile() . ':' . $e->getLine());
+        return false;
+    } finally {
+        print_line('Finally!');
+        return -1;
+    }
 }
+
+d(process());
