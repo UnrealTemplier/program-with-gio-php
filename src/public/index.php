f85d7bb938d561f82cc3e7d2ad5742c2a0fdbbad
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-11 20:11:08 +0400

    055 - DateTime: createFromFormat.

--- src/public/index.php
+++ src/public/index.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-$dateTime = new DateTime('03/15/2000 3:30PM');
+$date = '15/03/2033';
+$dateTime = DateTime::createFromFormat('d/m/Y', $date)->setTime(0, 0);
 
-print_line($dateTime->getTimezone()->getName() . ' - ' . $dateTime->format('m/d/Y g:i A'));
-
-$dateTime->setTimezone(new DateTimeZone('Europe/Amsterdam'))->setDate(2020, 05, 01)->setTime(14, 35);
-
-print_line($dateTime->getTimezone()->getName() . ' - ' . $dateTime->format('m/d/Y g:i A'));
+d($dateTime);
