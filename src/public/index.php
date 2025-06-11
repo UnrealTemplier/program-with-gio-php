85f9665000c5c157207404f8db477a522ca74358
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-11 18:08:24 +0400

    054 - Global exception handling: set_exception_handler function and handling all Throwable errors and exceptions.

--- src/public/index.php
+++ src/public/index.php
@@ -1,26 +1,12 @@
 <?php
 
-use App\Customer;
-use App\Exception\MissingBillingInfoException;
-use App\Order;
+declare(strict_types=1);
 
 require_once './../vendor/autoload.php';
 require_once './../helpers.php';
 
-function process(): int|bool
-{
-    $order1 = new Order(new Customer(), 25);
+set_exception_handler(function (\Throwable $throwable) {
+    print_line($throwable->getMessage());
+});
 
-    try {
-        $order1->process();
-        return true;
-    } catch (InvalidArgumentException|MissingBillingInfoException $e) {
-        print_line($e->getMessage() . ' ' . $e->getFile() . ':' . $e->getLine());
-        return false;
-    } finally {
-        print_line('Finally!');
-        return -1;
-    }
-}
-
-d(process());
+$a = 10 / 0;
