1dd743fdd4e10c34e188c25c28e30905d4207266
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-15 15:22:05 +0400

    065 - PDO: transactions.

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
-use JetBrains\PhpStorm\NoReturn;
 use PDO;
 use PDOException;
 
@@ -16,25 +15,7 @@ class HomeController
         try {
             $db = new PDO('mysql:host=mysql;dbname=my_db', 'root', 'root', [
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false
             ]);
-
-            $email = $_GET['email'];
-            $name = $_GET['name'];
-            $isActive = 1;
-            $createdAt = date('Y-m-d H:i:s', strtotime('2025-05-15 11:38:00'));
-            $query = 'insert into users (email, full_name, is_active, created_at) 
-                      values (:email, :name, :isActive, :createdAt)';
-
-            $stmt = $db->prepare($query);
-            $stmt->bindValue('email', $email);
-            $stmt->bindValue('name', $name);
-            $stmt->bindParam('isActive', $isActive);
-            $stmt->bindParam('createdAt', $createdAt);
-            $stmt->execute();
-
-            $id = $db->lastInsertId();
-            print_array($db->query('select * from users where id = ' . $id)->fetchAll());
         } catch (PDOException $e) {
             print_line(
                 'PDOException: ' . $e->getMessage(),
@@ -45,10 +26,49 @@ class HomeController
             );
         }
 
+        try {
+            $db->beginTransaction();
+
+            $email = 'j@doe.com';
+            $name = 'J Doe';
+            $isActive = 1;
+            $createdAt = date('Y-m-d H:i:s', time());
+            $updatedAt = date('Y-m-d H:i:s', time());
+
+            $amount = 25;
+
+            $newUserStmt = $db->prepare(
+                'insert into users (email, full_name, is_active, created_at, updated_at) 
+                 values (?, ?, ?, ?, ?)');
+
+            $newInvoiceStmt = $db->prepare(
+                'insert into invoices (amount, user_id)
+                 values (?, ?)'
+            );
+
+            $newUserStmt->execute([$email, $name, $isActive, $createdAt, $updatedAt]);
+
+            $userId = $db->lastInsertId();
+
+            $newInvoiceStmt->execute([$amount, $userId]);
+            
+            $db->commit();
+        } catch (\Throwable $e) {
+            if ($db->inTransaction()) {
+                $db->rollBack();
+            }
+
+            throw $e;
+        }
+
+        $query = 'select * from users where id = ?';
+        $stmt = $db->prepare($query);
+        $stmt->execute([$userId]);
+        print_array($stmt->fetchAll());
+
         return View::make('index');
     }
 
-    #[NoReturn]
     public function upload(): void
     {
         $fileData = $_FILES['receipt'];
