4e66caa859a506bde543887be43bd36f2c08e581
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-15 11:36:21 +0400

    064 - PDO: basic queries vulnerable to SQL injection.

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -5,14 +5,39 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
+use JetBrains\PhpStorm\NoReturn;
+use PDO;
+use PDOException;
 
 class HomeController
 {
     public function index(): View
     {
+        try {
+            $db = new PDO('mysql:host=mysql;dbname=my_db', 'root', 'root', [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+
+            $email = $_GET['email'];
+            $query = 'select * from users where email = "' . $email . '"';
+
+            print_line($query);
+
+            $stmt = $db->query($query);
+
+            print_array($stmt->fetchAll());
+        } catch (PDOException $e) {
+            print_line('PDOException: ' . $e->getMessage(),
+                'Error code: ' . $e->getCode(),
+                'In file: ' . $e->getFile(),
+                'At line: ' . $e->getLine(),
+                '');
+        }
+
         return View::make('index');
     }
 
+    #[NoReturn]
     public function upload(): void
     {
         $fileData = $_FILES['receipt'];
