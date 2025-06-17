4267007c212f8d1b8d78e0bb57e6bd3a300ab225
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-17 14:29:51 +0400

    069 - Add setUp method to our Router test.

--- src/tests/Unit/RouterTest.php
+++ src/tests/Unit/RouterTest.php
@@ -8,16 +8,22 @@ use App\Router;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
-class RouterTest extends TestCase
+final class RouterTest extends TestCase
 {
+    private Router $router;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->router = new Router();
+    }
+
     #[Test]
     public function it_registers_a_get_route(): void
     {
-        // given that we have a Router object
-        $router = new Router();
-
         // when we call a register method
-        $router->get('/users', ['Users', 'index']);
+        $this->router->get('/users', ['Users', 'index']);
 
         // then we assert route was registered
         $expected = [
@@ -26,17 +32,14 @@ class RouterTest extends TestCase
             ],
         ];
 
-        $this->assertEquals($expected, $router->routes);
+        $this->assertEquals($expected, $this->router->routes);
     }
 
     #[Test]
     public function it_registers_a_post_route(): void
     {
-        // given that we have a Router object
-        $router = new Router();
-
         // when we call a register method
-        $router->post('/users', ['Users', 'store']);
+        $this->router->post('/users', ['Users', 'store']);
 
         // then we assert route was registered
         $expected = [
@@ -45,18 +48,15 @@ class RouterTest extends TestCase
             ],
         ];
 
-        $this->assertEquals($expected, $router->routes);
+        $this->assertEquals($expected, $this->router->routes);
     }
 
     #[Test]
     public function it_registers_multiple_routes(): void
     {
-        // given that we have a Router object
-        $router = new Router();
-
         // when we call a register method
-        $router->get('/users', ['Users', 'index']);
-        $router->post('/users', ['Users', 'store']);
+        $this->router->get('/users', ['Users', 'index']);
+        $this->router->post('/users', ['Users', 'store']);
 
         // then we assert route was registered
         $expected = [
@@ -68,14 +68,12 @@ class RouterTest extends TestCase
             ],
         ];
 
-        $this->assertEquals($expected, $router->routes);
+        $this->assertEquals($expected, $this->router->routes);
     }
 
     #[Test]
     public function there_are_no_routes_when_router_created(): void
     {
-        $router = new Router();
-
-        $this->assertEmpty($router->routes);
+        $this->assertEmpty(new Router()->routes);
     }
 }
