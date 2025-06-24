8cc149e588830ae9ffd5c607b2130cb2ebce44ff
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-24 14:17:37 +0400

    085 - Add Doctrine ORM QueryBuilder custom expressions and join example.

--- src/scripts/doctrine_orm_example.php
+++ src/scripts/doctrine_orm_example.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use App\App;
 use App\Entity\Invoice;
+use App\Entity\InvoiceItem;
+use App\Enums\InvoiceStatus;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
@@ -31,21 +33,39 @@ $entityManager = new EntityManager($connection, $config);
 $queryBuilder = $entityManager->createQueryBuilder();
 
 $query = $queryBuilder
-    ->select('i')
+    ->select('i', 'items')
     ->from(Invoice::class, 'i')
-    ->where('i.amount > :amount')
+    ->join('i.items', 'items')
+    ->where(
+        $queryBuilder->expr()->andX(
+            $queryBuilder->expr()->gt('i.amount', ':amount'),
+            $queryBuilder->expr()->orX(
+                $queryBuilder->expr()->eq('i.status', ':status'),
+                $queryBuilder->expr()->gte('i.createdAt', ':date'),
+            ),
+        ),
+    )
     ->setParameter('amount', 40)
+    ->setParameter('status', InvoiceStatus::Pending)
+    ->setParameter('date', '2025-06-24 00:00:00')
     ->orderBy('i.createdAt', 'desc')
     ->getQuery();
 
-$invoices = $query->getArrayResult();
-print_array($invoices);
+$invoices = $query->getResult();
 
-//$invoices = $query->getResult();
+/** @var Invoice $invoice */
+foreach ($invoices as $invoice) {
+    echo $invoice->getCreatedAt()->format('d.m.Y G:i')
+        . ', ' . $invoice->getAmount()
+        . ', ' . $invoice->getStatus()->name . PHP_EOL;
 
-///** @var Invoice $invoice */
-//foreach ($invoices as $invoice) {
-//    echo ' - ' . $invoice->getCreatedAt()->format('d.m.Y G:i')
-//        . ', ' . $invoice->getAmount()
-//        . ', ' . $invoice->getStatus()->name . PHP_EOL;
-//}
+
+    /** @var InvoiceItem $item */
+    foreach ($invoice->getItems() as $item) {
+        echo ' - ' . $item->getDescription()
+            . ', ' . $item->getQuantity()
+            . ', ' . $item->getUnitPrice() . PHP_EOL;
+    }
+
+    echo PHP_EOL;
+}
