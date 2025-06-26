61453ff93a9515a075849b71ebb1de467bac64ed
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-26 11:48:54 +0400

    087 - Add basic Eloquent data creation example.

---
+++ src/App/Models/Invoice.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use App\Enums\InvoiceStatus;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * @property int $id
+ * @property float $amount
+ * @property string $invoice_number
+ * @property InvoiceStatus $status
+ * @property Carbon $created_at
+ * @property Carbon $due_date
+ */
+class Invoice extends Model
+{
+    const ?string UPDATED_AT = null;
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(InvoiceItem::class);
+    }
+}
--- src/scripts/eloquent_orm_example.php
+++ src/scripts/eloquent_orm_example.php
@@ -2,5 +2,32 @@
 
 declare(strict_types=1);
 
+use App\Enums\InvoiceStatus;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
+use Carbon\Carbon;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 require_once __DIR__ . '/../eloquent.php';
 
+Capsule::connection()->transaction(function () {
+    $invoice = new Invoice();
+    $invoice->amount = 57;
+    $invoice->invoice_number = '1';
+    $invoice->status = InvoiceStatus::Pending;
+    $invoice->due_date = new Carbon()->addDays(10);
+    $invoice->save();
+
+    $items = [['Item 1', 10, 4.55], ['Item 2', 20, 6.39], ['Item 3', 30, 7.89]];
+
+    foreach ($items as [$description, $quantity, $unitPrice]) {
+        $invoiceItem = new InvoiceItem();
+        $invoiceItem->description = $description;
+        $invoiceItem->quantity = $quantity;
+        $invoiceItem->unit_price = $unitPrice;
+
+        $invoiceItem->invoice()->associate($invoice);
+
+        $invoiceItem->save();
+    }
+});
