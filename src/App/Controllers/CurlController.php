9d1c43d7426bf8a985616726f7c0f6cb1b491051
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-27 21:09:30 +0400

    089 - Add basic API fetching with curl.

--- src/App/Controllers/CurlController.php
+++ src/App/Controllers/CurlController.php
@@ -3,28 +3,36 @@
 namespace App\Controllers;
 
 use App\Attributes\Get;
-use PHPUnit\Framework\Exception;
+use Exception;
 
 class CurlController
 {
     #[Get('/curl')]
     public function index(): void
     {
-        // some valid URL
-        $url = 'https://www.example.com';
+        $apiParams = [
+            'email' => 'jaimore.evseev@gmail.com',
+            'api_key' => 'test_6d8a9b27a6742a06ce84',
+        ];
 
-        // some invalid URL for error test
-        //$url = 'https://www.shit.com';
+        $api = 'https://api.emailable.com/v1/verify?' . http_build_query($apiParams);
+
+        $curlOptions = [
+            CURLOPT_URL => $api,
+            CURLOPT_RETURNTRANSFER => true,
+        ];
 
         $handle = curl_init();
-        curl_setopt($handle, CURLOPT_URL, $url);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt_array($handle, $curlOptions);
         $content = curl_exec($handle);
 
         if ($error = curl_error($handle)) {
-            throw new Exception('URL: ' . $url . '. Some shit happened!');
+            throw new Exception($error);
         }
 
-        echo $content;
+        if ($content !== false) {
+            $content = json_decode($content, true);
+            print_array($content);
+        }
     }
 }
