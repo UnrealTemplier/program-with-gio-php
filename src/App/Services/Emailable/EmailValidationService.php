5d6b050a3505383cdf459f211ba7759cb9a6d393
UnrealTemplier <jaimore.evseev@gmail.com>
2025-06-28 13:16:48 +0400

    090 - Add retrying mechanism to EmailValidationService.

--- src/App/Services/Emailable/EmailValidationService.php
+++ src/App/Services/Emailable/EmailValidationService.php
@@ -4,7 +4,12 @@ namespace App\Services\Emailable;
 
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class EmailValidationService
 {
@@ -17,9 +22,13 @@ class EmailValidationService
      */
     public function verify(string $email): array
     {
+        $stack = HandlerStack::create();
+        $stack->push($this->getRetryMiddleware(3));
+
         $client = new Client([
             'base_uri' => $this->baseUrl,
             'timeout' => 5,
+            'handler' => $stack,
         ]);
 
         $apiQueryParams = [
@@ -30,13 +39,36 @@ class EmailValidationService
         try {
             $response = $client->get('verify', ['query' => $apiQueryParams]);
             return json_decode($response->getBody()->getContents(), true);
-        }
-        catch (GuzzleException $e) {
-            echo $e->getMessage()
+        } catch (GuzzleException $e) {
+            echo 'Client error, code: ' . $e->getCode()
                 . ', in file: ' . $e->getFile()
                 . ', at line: ' . $e->getLine() . PHP_EOL;
         }
 
         return [];
+    }
+
+    private function getRetryMiddleware(int $maxRetries): callable
+    {
+        return Middleware::retry(function (
+            int $retries,
+            RequestInterface $request,
+            ?ResponseInterface $response = null,
+            ?\RuntimeException $e = null,
+        ) use ($maxRetries) {
+            if ($retries >= $maxRetries) {
+                return false;
+            }
+
+            if ($response && in_array($response->getStatusCode(), [249, 429, 503, 404])) {
+                return true;
+            }
+
+            if ($e instanceof ConnectException) {
+                return true;
+            }
+
+            return false;
+        });
     }
 }
